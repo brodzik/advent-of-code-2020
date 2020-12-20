d264a67e67f3b9730ddcfa41a210209322acbc32
Adrian Brodzik <adrianbrodzyk@gmail.com>
2020-12-20 02:17:57 +0100

    Add day 19

---
+++ Day 19/part_2.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func match1(rule []string, rules map[string][][]string, s string) bool {
+	for i, r := range rule {
+		if r[0] == '"' {
+			if len(s) > 0 && r[1] == s[0] {
+				s = s[1:]
+			} else {
+				return false
+			}
+		} else {
+			temp := [][]string{}
+
+			for _, x := range rules[r] {
+				for _, z := range rule[i+1:] {
+					x = append(x, z)
+				}
+
+				temp = append(temp, x)
+			}
+
+			return match(temp, rules, s)
+		}
+	}
+
+	return len(s) == 0
+}
+
+func match(rule [][]string, rules map[string][][]string, s string) bool {
+	for _, r := range rule {
+		if match1(r, rules, s) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func main() {
+	file, _ := os.Open("input.txt")
+	scanner := bufio.NewScanner(file)
+
+	rules := map[string][][]string{}
+	first := true
+	sum := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if strings.Contains(line, ":") {
+			temp1 := strings.Split(line, ": ")
+
+			subrules := [][]string{}
+
+			for _, x := range strings.Split(temp1[1], " | ") {
+				subrule := []string{}
+
+				for _, y := range strings.Split(x, " ") {
+					subrule = append(subrule, y)
+				}
+
+				subrules = append(subrules, subrule)
+			}
+
+			rules[temp1[0]] = subrules
+		} else {
+			if line != "" {
+				if first {
+					rules["8"] = [][]string{{"42"}, {"42", "8"}}
+					rules["11"] = [][]string{{"42", "31"}, {"42", "11", "31"}}
+					first = false
+				}
+
+				if match(rules["0"], rules, line) {
+					sum += 1
+				}
+			}
+		}
+	}
+
+	fmt.Println(sum)
+}
